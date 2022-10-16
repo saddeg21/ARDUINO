4873d9c86ce485cb19eac5997aa0ab72d481f2d2
Ahmet Deniz Daştan <89015461+saddeg21@users.noreply.github.com>
2022-10-16 16:43:58 +0300

    v1 committed

---
+++ BLINK LED/blink-led.ino
@@ -0,0 +1,13 @@
+#define LED 8
+void setup()
+{
+  pinMode(LED, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(LED, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(LED, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
